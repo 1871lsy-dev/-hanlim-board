869bc85170af64d1c939344ad46a649855fbb7c4
1871lsy-dev <1871lsy@gmail.com>
2026-03-27 01:26:50 +0900

    Update app.jsx

--- app.jsx
+++ app.jsx
@@ -488,12 +488,4 @@ const iS={width:"100%",boxSizing:"border-box",borderRadius:10,border:"1px solid 
   );
 }
 
-const FIREBASE_CONFIG = {
-  apiKey: "AIzaSyC04Ir2Jt7nZrrJGfGwSxUW0kd3sv7kwDA",
-  authDomain: "hanlim-board.firebaseapp.com",
-  databaseURL: "https://hanlim-board-default-rtdb.firebaseio.com",
-  projectId: "hanlim-board",
-  storageBucket: "hanlim-board.firebasestorage.app",
-  messagingSenderId: "663569376513",
-  appId: "1:663569376513:web:a3829d45c6d01bbfb1e700",
-};
+
